2387e6363275352f89888c0fe528d3c30ad51ac6
GHERBI HADJERMIMI <nh_gherbi@esi.dz>
2026-09-02 23:33:04 +0100

    Refactor index.js for improved organization; streamline imports and server initialization

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,36 +1,24 @@
-import express from "express";
-import cors from "cors";
-
+import express from 'express';
 import "dotenv/config";
-
+import { connectDB } from './lib/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import cors from "cors";
 import fs from "fs";
-import path from "path";
-import { clerkMiddleware } from "@clerk/express";
-import { connectDB } from "./lib/db.js";
-import job from "./lib/cron.js";
+import path from "path"
 
-import clerkWebhook from "./webhooks/clerk.webhook.js";
-import { app, server } from "./lib/socket.js";
+const app =express();
+console.log(process.env.DB_URL);
+const PORT =process.env.PORT;
+const FRONTED_URL =process.env.FRONTED_URL;
+const publicDir=path.join(process.cwd(),"public")
 
-const PORT = process.env.PORT;
-const FRONTEND_URL = process.env.FRONTEND_URL;
-
-const publicDir = path.join(process.cwd(), "public");
-
-// it's important that you don't parse the webhook event data, it should be in the raw format
-app.use("/api/webhooks/clerk", express.raw({ type: "application/json" }), clerkWebhook);
-
-app.use(express.json());
-app.use(cors({ origin: FRONTEND_URL, credentials: true }));
-app.use(clerkMiddleware());
-
-app.get("/health", (req, res) => {
-  res.status(200).json({ ok: true });
-});
-
-
-// if the public directory exists, serve the static files
-// this is for the production build
+app.use(express.json())
+app.use(cors({origin:FRONTED_URL,credentials:true}));
+app.use(clerkMiddleware())
+app.get("/health",(req,res)=>{
+    res.status(200).json({ok:true})
+})
+//this is for production buuild
 if (fs.existsSync(publicDir)) {
   app.use(express.static(publicDir));
 
@@ -38,10 +26,9 @@ if (fs.existsSync(publicDir)) {
     res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
   });
 }
+app.listen(PORT,()=> {
+    connectDB();
+    console.log("server is running on ",PORT);
+      if (process.env.NODE_ENV === "production") job.start();
 
-server.listen(PORT, () => {
-  connectDB();
-  console.log("Server is up and running on PORT:", PORT);
-
-  if (process.env.NODE_ENV === "production") job.start();
 });
