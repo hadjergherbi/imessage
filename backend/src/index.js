2a91e19a495955dc3f0a18a64c775c8a4131e63c
GHERBI HADJERMIMI <nh_gherbi@esi.dz>
2026-09-02 22:24:07 +0100

    Add Dockerfile for multi-stage build and create .dockerignore file

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,14 @@ import "dotenv/config";
 import { connectDB } from './lib/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import cors from "cors";
+import fs from "fs";
+import path from "path"
 
 const app =express();
 console.log(process.env.DB_URL);
 const PORT =process.env.PORT;
 const FRONTED_URL =process.env.FRONTED_URL;
-
+const publicDir=path.join(process.cwd(),"public")
 
 app.use(express.json())
 app.use(cors({origin:FRONTED_URL,credentials:true}));
@@ -16,6 +18,14 @@ app.use(clerkMiddleware())
 app.get("/health",(req,res)=>{
     res.status(200).json({ok:true})
 })
+//this is for production buuild
+if (fs.existsSync(publicDir)) {
+  app.use(express.static(publicDir));
+
+  app.get("/{*any}", (req, res, next) => {
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  });
+}
 app.listen(PORT,()=> {
     connectDB();
     console.log("server is running on ",PORT)});
